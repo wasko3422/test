fce47bd006643ded4e23481ed190493279ca2a07
Egor Panov <panow.egor2018@gmail.com>
2019-07-10 21:00:54 +0300

    added docstrings

--- assignment.py
+++ assignment.py
@@ -3,8 +3,12 @@ import numpy as np
 
 
 class Assignment:
+    """
+    The assignment class provides the calculation methods.
+    """
+
     day = pd.to_timedelta("1day")
-    days = pd.to_timedelta("15day")
+    days = pd.to_timedelta("5day")
 
     def __init__(self):
         self.prices = pd.read_csv("data//prices.csv", index_col=0)
@@ -30,6 +34,29 @@ class Assignment:
         self.prices.index = pd.to_datetime(self.prices.index)
 
     def calculate_asset_performance(self, start_date: np.datetime64, end_date: np.datetime64) -> pd.Series:
+        """
+        Return a series with an asset performance from start_date to end_date
+
+        Parameters
+        -----------
+        start_date: datetime_like
+        end_date: datetime_like
+
+        Returns
+        -----------
+        out : pandas.Series
+
+        Notes
+        -----------
+        pt - current price
+        pt0 - price over the previous period
+        w - weights
+        r - asset return
+        rs - weighted asset return
+        p - asset performance
+
+        """
+
         end_date += self.day
         pt = self.prices[start_date:end_date]
 
@@ -57,6 +84,29 @@ class Assignment:
         return pd.Series(p[1:])
 
     def calculate_currency_performance(self, start_date: np.datetime64, end_date: np.datetime64) -> pd.Series:
+        """
+        Return a series with a currency performance from start_date to end_date
+
+        Parameters
+        -----------
+        start_date: datetime_like
+        end_date: datetime_like
+
+        Returns
+        -----------
+        out : pandas.Series
+
+        Notes
+        -----------
+        ct - exchange rate
+        ct0 - exchange rate over the previous period
+        w - weights
+        cr - currency return
+        rs - weighted currency return
+        cp - currency performance
+
+        """
+
         end_date += self.day
         ct = self.exchanges[start_date:end_date]
         ct0 = self.exchanges[start_date-self.days:end_date].values
@@ -79,6 +129,31 @@ class Assignment:
         return pd.Series(cp[1:])
 
     def calculate_total_performance(self, start_date: np.datetime64, end_date: np.datetime64) -> pd.Series:
+        """
+        Return a series with a currency performance from start_date to end_date
+
+        Parameters
+        -----------
+        start_date: datetime_like
+        end_date: datetime_like
+
+        Returns
+        -----------
+        out : pandas.Series
+
+        Notes
+        -----------
+        p - prices
+        c - exchange rates
+        ptc - prices multiplied by exchange rates 
+        ptc0 - prices over the previous period multiplied by exchange rater
+        w - weights
+        tr - total return
+        trs - weighted total return
+        tp - total performance
+
+        """
+
         end_date += self.day
         # To prevent SettingWithCopyWarning
         p = self.prices[start_date-self.days:end_date].copy()
